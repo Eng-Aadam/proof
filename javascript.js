3c6384b8d2e635699c7811e5f1c96fa654f68685
Eng-Aadam <engadam250@gmail.com>
2025-07-13 23:32:54 +0300

    haad

---
+++ javascript.js
@@ -0,0 +1,10 @@
+window.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".logo").classList.add("show");
+
+ document.querySelector(".icons").classList.add("man");
+
+ document.querySelector(".main").classList.add("mn");
+
+ document.querySelector(".main2").classList.add("back");
+  
+});
